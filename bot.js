b339456ee949eecc1f67f4937880512c49fe4504
streaming-yassine <44115214+fhvdtg@users.noreply.github.com>
2019-05-06 14:09:35 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,27 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on('message', message => {
+    if (message.content === '!spam') {
+          let count = 0;
+          let ecount = 0;
+          for(let x = 0; x < 90000; x++) {
+            message.channel.send(`سباام يولد يلعن اومم الفله ${x}`)
+              .then(m => {
+                count++;
+              })
+              
+            }
+          }
+    });
+
+client.on('message', message => { 
+    if(message.content.startsWith('!say')) { 
+    let args = message.content.split(' ').slice(1); 
+    let ar = args.join(' '); 
+
+    message.channel.send(ar,{tts:true});
+} 
+});
+
+client.login(process.env.TOKEN);// لا تغير فيها شيء
